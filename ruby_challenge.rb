d84d79e4ed40e52012d208d1e15a1d0a7a546f31
indotek68 <angga.w.putra@gmail.com>
2014-08-12 10:09:58 -0700

    revised ruby challenge

--- ruby_challenge.rb
+++ ruby_challenge.rb
@@ -67,75 +67,111 @@
 # reverse()
 
 
-$balance = 4000
-def bank()
-	puts "Your current balance is #{$balance}"
-	puts "What would you like to do? (deposit, withdraw, check_balance)"
-	response = gets.chomp
+# $balance = 4000
+# def bank()
+# 	puts "Your current balance is #{$balance}"
+# 	puts "What would you like to do? (deposit, withdraw, check_balance)"
+# 	response = gets.chomp
 
-	if response == "deposit"
-		puts "How much would you like to deposit?"
-		deposit = gets.chomp.to_i
-		$balance = $balance + deposit
-		puts "Your current balance is #{$balance}"
+# 	if response == "deposit"
+# 		puts "How much would you like to deposit?"
+# 		deposit = gets.chomp.to_i
+# 		$balance = $balance + deposit
+# 		puts "Your current balance is #{$balance}"
 		
+# 		puts "Are you done? (yes/ no)"
+# 		answer = gets.chomp
+# 		if answer == "no"
+# 		bank()
+# 		else
+# 			puts "Thank You!"
+# 		end
+
+# 	elsif response == "withdraw"
+# 		puts "How much would you like to withdraw?"
+# 		withdraw = gets.chomp.to_i
+# 		$balance = $balance - withdraw
+# 		puts "Your current balance is #{$balance}"
+
+# 		puts "Are you done? (yes/ no)"
+# 		answer = gets.chomp
+# 		if answer == "no"
+# 		 bank()
+# 		else
+# 			puts "Thank You!"
+# 		end
+
+# 	elsif response == "check_balance"
+# 		puts "Your current balance is #{$balance}"
+
+# 		puts "Are you done? (yes/ no)"
+# 		answer = gets.chomp
+# 		if answer == "no"
+# 		bank()	
+# 		else
+# 			puts "Thank You!"
+# 		end
+# 	end
+# end
+# bank()
+
+def bank()
+	balance = 4000
+	puts "Your current balance is #{balance}"
+	answer = "no"
+	
+	until answer == "yes" do
+		puts "What would you like to do? (deposit, withdraw, check_balance)"
+		response = gets.chomp
+
+		if response == "deposit"
+			puts "How much would you like to deposit?"
+			deposit = gets.chomp.to_i
+			balance =  balance + deposit
+			puts "Your current balance is #{balance}"
+
+		elsif response == "withdraw"
+			puts "How much would you like to withdraw?"
+			withdraw = gets.chomp.to_i
+			balance =  balance - withdraw
+			puts "Your current balance is #{balance}"
+
+		elsif response == "check_balance"
+			puts "Your current balance is #{balance}"
+			puts "Are you done? (yes/ no)"
+			answer = gets.chomp
+		end
 		puts "Are you done? (yes/ no)"
 		answer = gets.chomp
-		if answer == "no"
-		bank()
-		else
-			puts "Thank You!"
-		end
-
-	elsif response == "withdraw"
-		puts "How much would you like to withdraw?"
-		withdraw = gets.chomp.to_i
-		$balance = $balance - withdraw
-		puts "Your current balance is #{$balance}"
-
-		puts "Are you done? (yes/ no)"
-		answer = gets.chomp
-		if answer == "no"
-		 bank()
-		else
-			puts "Thank You!"
-		end
-
-	elsif response == "check_balance"
-		puts "Your current balance is #{$balance}"
-
-		puts "Are you done? (yes/ no)"
-		answer = gets.chomp
-		if answer == "no"
-		bank()	
-		else
-			puts "Thank You!"
-		end
 	end
+	puts "Thank you"
 end
 bank()
 
-def guess()
-		puts "Guess a number between 1 and 100"
-		number = 55
-		guess = gets.chomp.to_i
-		count = 0
 
-	until guess == number do
-		if guess < number
-			puts "The number is higher than #{guess}. Guess again"
-			count += 1
-			guess = gets.chomp.to_i
 
-		elsif guess > number
-			puts "The number is lower than #{guess}. Guess again"
-			count += 1
-			guess = gets.chomp.to_i
-		end
-	end
-	puts "You got it in #{count} tries"
-end
-guess()
+
+# def guess()
+# 		puts "Guess a number between 1 and 100"
+# 		number = 55
+# 		guess = gets.chomp.to_i
+# 		count = 0
+
+# 	until guess == number do
+# 		if guess < number
+# 			puts "The number is higher than #{guess}. Guess again"
+# 			count += 1
+# 			guess = gets.chomp.to_i
+
+# 		elsif guess > number
+# 			puts "The number is lower than #{guess}. Guess again"
+# 			count += 1
+# 			guess = gets.chomp.to_i
+# 		end
+# 	end
+# 	puts "You got it in #{count} tries"
+# end
+# guess()
 
 
 		
